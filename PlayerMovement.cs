c36ac86fdde762e5a0b4a2e5b004a849a63e0b44
coreyg96 <60311329+coreyg96@users.noreply.github.com>
2020-11-08 07:51:48 -0500

    Create PlayerMovement.cs
    Player Movement scripted created for a 2d environment, also includes part of a collectable script.

---
+++ PlayerMovement.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    public List<string> inventory;
+
+    void Start()
+    {
+        inventory = new List<string>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Collectable"))
+        {
+            string itemType = collision.gameObject.GetComponent<CollectableScript>().itemType;
+
+            print("We have collected a:" + itemType);
+
+            inventory.Add(itemType);
+            print("Inventory length:" + inventory.Count);
+
+            Destroy(collision.gameObject);
+        }
+
+
+    }
+}
